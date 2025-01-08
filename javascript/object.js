8cdc57083e4bffc7fa0c0bbc5089738e05444b37
jinsol kim <jinsol032360@gmail.com>
2025-01-08 21:18:42 +0900

    객체의 특징징

---
+++ javascript/object.js
@@ -0,0 +1,17 @@
+// 객체 선언하기
+const classmates = {
+  name: "철수",
+  age: 8,
+  school: "꽃샘초등학교"
+}
+
+// 객체의 값 꺼내기
+classmates.name
+classmates['name']
+
+// 객체의 키&값 삭제하기
+delete classmates.name
+// classmates = {
+//   age: 8,
+//   school: "꽃샘초등학교교"
+// }
